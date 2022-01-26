5ae957d4729b4601df36dfc99dfb36df5367e1b9
Marti <marti.jimenez.2002@gmail.com>
2022-01-26 20:11:24 +0100

    Petites modifications de syntaxe d'utilisation de packages et commentaires

--- projet.py
+++ projet.py
@@ -6,31 +6,34 @@ from matplotlib import pyplot as plt
 
 
 def show_fourier_graph_compare(PATH1 , PATH2):
-    plot1 = plt.figure(1)
+    
+    #calcul fft figure 2
     s_rate , signal = wf.read(PATH1)
-
-
-    FFT = abs(scipy.fft.fft(signal))
+    FFT = np.abs(fftpk.fft(signal))
     freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))
-
+    
+    # plot de la figure 1
+    _ = plt.figure(1)
     plt.plot(freqs[range(len(FFT)//2)] , FFT[range(len(FFT)//2)])
     plt.xlabel("Frequence (Hz)")
     plt.ylabel("Amplitude")
     plt.title(PATH1)
-    ############################################
-    plot2 = plt.figure(2)
-    s_rate , signal = wf.read(PATH2)
     
 
-    FFT = np.abs(scipy.fft.fft(signal))
+    #calcul fft figure 2
+    s_rate , signal = wf.read(PATH2)
+    FFT = np.abs(fftpk.fft(signal))
     freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))
-
+    
+    
+    # plot de la figure 2
+    _ = plt.figure(2)
     plt.plot(freqs[range(len(FFT)//2)] , FFT[range(len(FFT)//2)])
     plt.xlabel("Frequence (Hz)")
     plt.ylabel("Amplitude")
     plt.title(PATH2)
 
-
+    #afficher les figures
     plt.show()
 
 fem1 = "C:\\Users\\marti\\Desktop\\fem1.wav"
@@ -38,4 +41,4 @@ vem1 = "C:\\Users\\marti\\Desktop\\vem1.wav"
 fft1 = "C:\\Users\\marti\\Desktop\\fft1.wav"
 vvt1 = "C:\\Users\\marti\\Desktop\\vvt1.wav"
 
-show_fourier_graph_compare(fft1,vvt1)
+show_fourier_graph_compare(fft1,fft1)
