62c300250eb5a3d8684de3b61fe858f7a391961b
Marti <marti.jimenez.2002@gmail.com>
2022-01-30 18:03:36 +0100

    Implémentation moyennage à 0.5

--- projet.py
+++ projet.py
@@ -26,16 +26,62 @@ def show_fourier_graph_compare(PATH1 , PATH2):
     s_rate , signal = wf.read(PATH2)
     FFT = np.abs(fftpk.fft(signal))
     freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))
+
+
+    print("Freq:" , freqs[:20])
+    print("FFT :" , FFT[:20])
+
+    a = 0.5
+    res = []
+    sum = 0
+    den = 0
+    for i,f in enumerate(freqs[range(len(FFT)//2)]):
+        if f <= a:
+            sum += FFT[i][0]
+            den+=1
+        else:
+            if den == 0:
+                res.append(0)
+            else:
+                res.append(sum/den)
+            
+            if f <= a + 0.5 :
+                sum = FFT[i][0]
+                den = 1
+                a +=0.5
+            else :
+                res.append(0)
+                sum = 0
+                den = 0
+                a +=1
+    
+    freqs = np.arange(0.0,20000,0.5)
+    print("frequences: " , freqs[:20])
+    print("res: " ,res[:20])
     
     
+
+
+
+
     # plot de la figure 2
     _ = plt.figure(2)
-    plt.plot(freqs[range(len(FFT)//2)] , FFT[range(len(FFT)//2)])
+    
+    while len(res) < 40000:
+        res.append(0)
+
+    plt.plot(freqs, res[:40000])
+    
+    #plt.plot(freqs[range(len(FFT)//2)] , FFT[range(len(FFT)//2)])
     plt.xlabel("Frequence (Hz)")
     plt.ylabel("Amplitude")
     plt.xlim([0,20000])
     plt.ylim(bottom=0)
-    plt.title(PATH2)
+    #plt.title(PATH2)
+    plt.title("Moyennage a 0.5")
+
+
+
 
     #afficher les figures
     plt.show()
@@ -46,3 +92,6 @@ fft1 = "C:\\Users\\marti\\Desktop\\fft1.wav"
 vvt1 = "C:\\Users\\marti\\Desktop\\vvt1.wav"
 
 show_fourier_graph_compare(fft1,fft1)
+show_fourier_graph_compare(vvt1,vvt1)
+show_fourier_graph_compare(fem1,fem1)
+show_fourier_graph_compare(vem1,vem1)
