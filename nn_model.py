dc45d7cb510453f97af1f4a935aff01eeade5184
Marti <marti.jimenez.2002@gmail.com>
2022-02-02 15:14:29 +0100

    v2.2.22

--- nn_model.py
+++ nn_model.py
@@ -1,40 +1,57 @@
-from cmath import exp
 import numpy as np
-from create_nn_input import Matvvt1
+
+
+test_input = [
+    np.array([0,1]),np.array([1,0]),np.array([1,1]),np.array([0,0])
+]
+
+test_output = [1,1,0,0]
 
 class Neural_Network(object):
     def __init__(self):
-        self.inputSize = 40000
-        self.outputSize = 2
-        self.hidden1Size = 20000
-        self.hidden2Size = 20000
+        self.inputSize = 2
+        self.outputSize = 1
+        self.hiddenSize = 3
+        #self.hidden2Size = 20000
 
-        self.w1 = np.random.randn(self.inputSize,self.hidden1Size) #Mat(40000,80000)
-        self.w2 = np.random.randn(self.hidden1Size,self.hidden2Size) #Mat(80000,80000)
-        self.w3 = np.random.randn(self.hidden2Size,self.outputSize) #Mat(80000,2)
+        self.w1 = np.random.randn(self.inputSize,self.hiddenSize) #Mat(40000,80000)
+        self.w2 = np.random.randn(self.hiddenSize,self.outputSize) #Mat(80000,80000)
+        #self.w3 = np.random.randn(self.hidden2Size,self.outputSize) #Mat(80000,2)
 
     def forward(self,X):
 
         self.l1 = np.dot(X, self.w1)
         self.al1 = self.sigmoid(self.l1)
 
-        self.l2 = np.dot(self.al1,self.w2)
-        self.al2 = self.sigmoid(self.l2)
+        self.o = np.dot(self.al1,self.w2)
 
-        self.o = np.dot(self.al2,self.w3)
-        ao = self.sigmoid(self.o)
+        output = self.sigmoid(self.o)
 
-        return ao
+        return output
+
+        #self.l2 = np.dot(self.al1,self.w2)
+        #self.al2 = self.sigmoid(self.l2)
+
+        #self.o = np.dot(self.al2,self.w3)
+        #ao = self.sigmoid(self.o)
+
+        #return ao
     
     def sigmoid(self,s):
-        return 1/(1+exp(-s))
+        return 1/(1+np.exp(-s))
 
     def sigmoid_prime(self,s):
         return self.sigmoid(s)*(1-self.sigmoid(s))
 
+    def train (self, input , expected_output , step):
+        output = self.forward(input)
+        cost = (abs(expected_output - output)**2)/2
+
+
+
 NN1 = Neural_Network()
 
-print(NN1.forward(Matvvt1))
+print(NN1.forward(test_input[0]))
 
 
         
--- show_averaged_fft_graph.py
+++ show_averaged_fft_graph.py
@@ -3,71 +3,113 @@ import scipy
 import scipy.fftpack as fftpk
 import numpy as np
 from matplotlib import pyplot as plt
-import os
+import os , sys
+import math
 
-# this procedure shows the averaged fft graph of the .wav input file
-def show_averaged_fourier_graph(PATH):
-    
+# class to remove output text for a command
+class suppress_output:
+    def __init__(self, suppress_stdout=False, suppress_stderr=False):
+        self.suppress_stdout = suppress_stdout
+        self.suppress_stderr = suppress_stderr
+        self._stdout = None
+        self._stderr = None
 
-    #fast fourier transform of input signal
-    s_rate , signal = wf.read(PATH)
-    FFT = np.abs(fftpk.fft(signal))
-    freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))
+    def __enter__(self):
+        devnull = open(os.devnull, "w")
+        if self.suppress_stdout:
+            self._stdout = sys.stdout
+            sys.stdout = devnull
 
+        if self.suppress_stderr:
+            self._stderr = sys.stderr
+            sys.stderr = devnull
 
-    # doing an average of the values between the frequences n and n+0.5
-    a = 0.5
-    res = []
-    sum = 0
-    den = 0
-    for i,f in enumerate(freqs[range(len(FFT)//2)]):
-        if f <= a:
-            sum += FFT[i][0]
-            den+=1
-        else:
-            if den == 0:
-                res.append(0)
+    def __exit__(self, *args):
+        if self.suppress_stdout:
+            sys.stdout = self._stdout
+        if self.suppress_stderr:
+            sys.stderr = self._stderr
+
+# this procedure shows the averaged fft graph of the .wav input file // ->(number_of_files_to_compare , file_1, file_2 ,..., file_n)
+def show_averaged_fourier_graph(n,*args):
+
+    # for each .wav file 
+    for graph in range(1,n+1):
+
+        #fast fourier transform of input signal
+
+        with suppress_output(suppress_stdout=True, suppress_stderr=True):
+            #we don't print the output message of this function
+            s_rate , signal = wf.read(args[graph-1])
+        
+        FFT = np.abs(fftpk.fft(signal))
+        freqs = fftpk.fftfreq(len(FFT), (1.0/s_rate))
+        
+        try :
+            FFT = FFT[:,0]
+        except :
+            pass
+        
+        
+        # doing an average of the values between the frequences n and n+0.5
+        a = 0.5
+        res = []
+        sum = 0
+        den = 0
+        for i,f in enumerate(freqs[range(len(FFT)//2)]):
+            if f <= a:
+                sum += FFT[i]
+                den+=1
             else:
-                res.append(sum/den)
-            
-            if f <= a + 0.5 :
-                sum = FFT[i][0]
-                den = 1
-                a +=0.5
-            else :
-                res.append(0)
-                sum = 0
-                den = 0
-                a +=1
-    
-    # new frequences for res, you can use them to visualise the graph of the output with matplotlib
-    freqs = np.arange(0.0,20000,0.5)
-    
-    # plot of the figure
-    
-    while len(res) < 40000:
-        res.append(0)
-
-    plt.plot(freqs, res[:40000])
-
-    plt.xlabel("Frequence (Hz)")
-    plt.ylabel("Amplitude")
-    plt.xlim([0,20000])
-    plt.ylim(bottom=0)
-    plt.title(PATH)
+                if den == 0:
+                    res.append(0)
+                else:
+                    res.append(sum/den)
+                
+                if f <= a + 0.5 :
+                    sum = FFT[i]
+                    den = 1
+                    a +=0.5
+                else :
+                    res.append(0)
+                    sum = 0
+                    den = 0
+                    a +=1
+        
+        # new frequences for res, used to visualise the graph of the output with matplotlib
+        freqs = np.arange(0.0,20000,0.5)
+        while len(res) < 40000:
+            res.append(0)
+        
+        # plot of the figure n
+        plt.subplot(math.ceil(n/math.ceil(n/2)),math.ceil(n/2),graph)
+        plt.plot(freqs, res[:40000])
+        plt.xlabel("Frequence (Hz)")
+        plt.ylabel("Amplitude")
+        plt.xlim([0,1500])
+        plt.ylim(bottom = 0)
+        plt.title(((args[graph-1]).split("\\")[-1]).split(".")[0])
+        
+    graphs = [((args[i]).split("\\")[-1]).split(".")[0] for i in range(graph)]
+    print("Showing graphs of :" , *graphs, sep=' | ')
 
     #show figure
     plt.show()
 
 
 # test with some audio samples
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-fft1 = os.path.join(__location__, 'audio_samples\\fft1.wav')
-vvt1 = os.path.join(__location__, 'audio_samples\\vvt1.wav')
-fem1 = os.path.join(__location__, 'audio_samples\\fem1.wav')
-vem1 = os.path.join(__location__, 'audio_samples\\vem1.wav')
+def get_loc (nom):
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    return os.path.join(__location__, f'audio_samples\\{nom}.wav')
+
+# Disponible samples
+fichiers = []
+for fichier in os.listdir("C:\\Users\\marti\\Desktop\\Dossiers\\Coding\\Projet 2MIC_S2\\audio_samples"):
+    fichiers.append(fichier.split(".")[0])
+
+print("Disponible samples :", *fichiers, sep=" | ")
 
 # showing the fft averaged graph
-show_averaged_fourier_graph(fft1)
+show_averaged_fourier_graph(8,get_loc("v1"),get_loc("f1"),get_loc("vet"),get_loc("fet"),get_loc("v2"),get_loc("f2"),get_loc("v3"),get_loc("f3"))
 
